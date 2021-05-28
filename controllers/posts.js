19139156bdb8038e89ba893f5664b6a7183f1082
Sudha08 <sudha0894@gmail.com>
2021-05-29 00:47:01 +0530

    Add map to the view, edit location

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,8 @@
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
+
 cloudinary.config({
   cloud_name: 'sudha-cloudinary',
   api_key: '237896876235567',
@@ -26,6 +29,13 @@ module.exports = {
         public_id: image.public_id
       });
     }
+    let response = await geocodingClient
+      .forwardGeocode({
+      query: req.body.post.location,
+      limit: 1
+      })
+      .send();
+    req.body.post.coordinates = response.body.features[0].geometry.coordinates;
     let post = await Post.create(req.body.post);
     res.redirect(`/posts/${post.id}`);
   },
@@ -71,12 +81,22 @@ module.exports = {
           public_id: image.public_id
         });
       }
-    }			
+    }
+    // check if location is updated
+    if (req.body.post.location !== post.location) {
+      let response = await geocodingClient
+      .forwardGeocode({
+      query: req.body.post.location,
+      limit: 1
+      })
+      .send();
+      post.coordinates = response.body.features[0].geometry.coordinates;
+      post.location = req.body.post.location;
+    }
     // update the post with any new properties
     post.title = req.body.post.title;
     post.price = req.body.post.price;
     post.description = req.body.post.description;
-    post.location = req.body.post.location;
     // save the updated post into the db
     post.save();
 	  // redirect to show page
